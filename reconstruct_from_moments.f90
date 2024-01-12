c25d716eebd10b27b4d30455675ed19b9eaf5c1a
Daniel Price <daniel.price@monash.edu>
2024-01-12 14:20:20 +1100

    (reconstruct) added Gauss-Legendre integration to speed things up

--- reconstruct_from_moments.f90
+++ reconstruct_from_moments.f90
@@ -6,6 +6,7 @@ module reconstruct_from_moments
  public :: reconstruct,reconstruct_maxent,fsolve_error,integrand
 
  logical, private :: use_log_grid = .false.
+ logical, private :: log_grid_is_uniform = .false.
  private
 
 contains
@@ -32,12 +33,13 @@ subroutine reconstruct(mu,x,f,lambsol,ierr,lambguess)
  real, intent(out) :: lambsol(size(mu))
  integer, intent(out) :: ierr
  real, intent(in), optional :: lambguess(size(mu))
+ real :: lsum(size(mu))
 
  ! in principle can choose method here
  if (present(lambguess)) then
-    call reconstruct_maxent(mu,x,f,lambsol,ierr,lambguess)
+    call reconstruct_maxent(mu,x,f,lambsol,lsum,ierr,lambguess)
  else
-    call reconstruct_maxent(mu,x,f,lambsol,ierr,lambguess)
+    call reconstruct_maxent(mu,x,f,lambsol,lsum,ierr)
  endif
 
 end subroutine reconstruct
@@ -45,17 +47,22 @@ end subroutine reconstruct
 !
 ! maximum entropy reconstruction of function given moments
 !
-subroutine reconstruct_maxent(mu,x,f,lambsol,ierr,lambguess,use_log)
+subroutine reconstruct_maxent(mu,x,f,lambsol,lsum,ierr,lambguess,use_log,weights)
+ use integrate, only:check_log_grid_is_uniform
  real, intent(in) :: mu(:)
  real, intent(in) :: x(:)
  real, intent(out) :: f(size(x))
  real, intent(out) :: lambsol(size(mu))
+ real, intent(out) :: lsum(size(mu))
  integer, intent(out) :: ierr
  real, intent(in), optional :: lambguess(size(mu))
+ real, intent(in), optional :: weights(size(x))
  logical, intent(in), optional :: use_log
  integer :: n_moments
  real, parameter :: tol = 1.e-6
- real :: lsum(size(mu))
+ real, allocatable :: logx(:)
+ logical :: use_gauss_legendre = .true.
+
 
  lambsol = 0.
  ierr = 0
@@ -66,12 +73,27 @@ subroutine reconstruct_maxent(mu,x,f,lambsol,ierr,lambguess,use_log)
  if (present(lambguess)) then
     ! use guesses passed as arguments (e.g. from previous attempt)
     lambsol = lambguess
+    !print*,' using lambguess = ',lambguess
  else
     ! use predefined guess
     lambsol(1) = -log(sqrt(2.*pi))
  endif
+
+ !
+ ! if we are using a log grid, precalculate the grid
+ ! of logx (this is an optimisation)
+ !
  use_log_grid = .false.
  if (present(use_log)) use_log_grid = use_log
+ if (use_log_grid) then
+    log_grid_is_uniform = check_log_grid_is_uniform(x)
+    allocate(logx(size(x)))
+    logx = log(x)
+ endif
+ !
+ ! if pre-calculated weights are sent, use Gauss-Legendre quadrature
+ !
+ use_gauss_legendre = present(weights)
 
  call fsolve(residual,n_moments,lambsol,lsum,tol,ierr)
  f = integrand(x,lambsol,n_moments)
@@ -92,21 +114,33 @@ contains
 !        rhs: the integrated right hand side of the moment approximation function
 !
 subroutine residual(n,lamb,l_sum)
- use integrate, only:integrate_trap,integrate_trap_log
+ use integrate, only:integrate_trap,integrate_trap_log,integrate_trap_log_uniform,integrate_gauss_legendre
  integer, intent(in) :: n
  real ( kind = rk ), intent(in)  :: lamb(n) ! guess for  solution
  real ( kind = rk ), intent(out) :: l_sum(n) ! function evaluated for given lamb
 
  integer :: k
 
- if (use_log_grid) then
+ if (use_gauss_legendre) then
     do k=1,n
-       l_sum(k) = integrate_trap_log(size(x),x,integrand(x,lamb,k-1)) - mu(k)
+       l_sum(k) = integrate_gauss_legendre(size(x),weights,integrand(x,lamb,k-1,logx=logx)) - mu(k)
     enddo
  else
-    do k=1,n
-       l_sum(k) = integrate_trap(size(x),x,integrand(x,lamb,k-1)) - mu(k)
-    enddo
+    if (use_log_grid) then
+       if (log_grid_is_uniform) then
+          do k=1,n
+             l_sum(k) = integrate_trap_log_uniform(size(x),x,integrand(x,lamb,k-1,logx=logx)) - mu(k)
+          enddo
+       else
+          do k=1,n
+             l_sum(k) = integrate_trap_log(size(x),x,integrand(x,lamb,k-1,logx=logx)) - mu(k)
+          enddo
+       endif
+    else
+       do k=1,n
+          l_sum(k) = integrate_trap(size(x),x,integrand(x,lamb,k-1)) - mu(k)
+       enddo
+    endif
  endif
  !print*,' moments are: ',l_sum(:) + mu(:)
  !print*,' residuals are: ',l_sum(:)
@@ -123,19 +157,26 @@ end subroutine reconstruct_maxent
 !    lamb: array of Lagrange multipliers
 !    k   : order of moment to calculate (e.g. 0, 1, 2...)
 !
-function integrand(x,lamb,k) result(y)
+function integrand(x,lamb,k,logx) result(y)
  real( kind = rk ), intent(in) :: x(:)
  real( kind = rk ), intent(in) :: lamb(:)
  real( kind = rk ) :: y(size(x))  ! result
+ real( kind = rk ), intent(in), optional :: logx(:)
  integer, intent(in) :: k
- real( kind = rk ) :: xi(size(lamb))
+ real( kind = rk ) :: xi(size(lamb)), term
  integer :: i,j
 
  do i=1,size(x)
     if (use_log_grid) then
-       do j=1,size(lamb)
-          xi(j) = log(x(i))**(j-1)
-       enddo
+       if (present(logx)) then
+          do j=1,size(lamb)
+             xi(j) = logx(i)**(j-1)
+          enddo
+       else
+          do j=1,size(lamb)
+             xi(j) = log(x(i))**(j-1)
+          enddo
+       endif
     else
        do j=1,size(lamb)
           xi(j) = x(i)**(j-1)
@@ -143,7 +184,13 @@ function integrand(x,lamb,k) result(y)
     endif
     ! function is exp( lam_0 + lam_1 x + lam_2 x^2 + ...)
     ! or if log_grid is set exp( lam_0 + lam_1 log(x) + lam_2 log(x)^2 + ...)
-    y(i) = x(i)**k * exp(dot_product(lamb,xi))
+    term = dot_product(lamb,xi)
+    if (term < -50.) then ! avoid wasted effort on exp(-100) etc
+       y(i) = 0.
+    else
+       y(i) = x(i)**(k/3.)*exp(term)
+    endif
+    !y(i) = x(i)**(k/3.) * exp(max(dot_product(lamb,xi),-100.))
  enddo
 
 end function integrand
