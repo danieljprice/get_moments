3eb78b4d247dd94e29ad455ed892bc9854c5d4b5
Daniel Price <daniel.price@monash.edu>
2024-01-15 18:15:52 +1100

    moment reconstruction by fitting generalised gamma function now working

--- reconstruct_from_moments.f90
+++ reconstruct_from_moments.f90
@@ -4,6 +4,7 @@ module reconstruct_from_moments
  integer, parameter :: rk = kind(0.d0)
 
  public :: reconstruct,reconstruct_maxent,fsolve_error,integrand
+ public :: reconstruct_gamma_dist
 
  logical, private :: use_log_grid = .false.
  logical, private :: log_grid_is_uniform = .false.
@@ -59,7 +60,7 @@ subroutine reconstruct_maxent(mu,x,f,lambsol,lsum,ierr,lambguess,use_log,weights
  real, intent(in), optional :: weights(size(x))
  logical, intent(in), optional :: use_log
  integer :: n_moments
- real, parameter :: tol = 1.e-6
+ real, parameter :: tol = 1.e-10
  real, allocatable :: logx(:)
  logical :: use_gauss_legendre = .true.
 
@@ -118,13 +119,14 @@ subroutine residual(n,lamb,l_sum)
  integer, intent(in) :: n
  real ( kind = rk ), intent(in)  :: lamb(n) ! guess for  solution
  real ( kind = rk ), intent(out) :: l_sum(n) ! function evaluated for given lamb
-
  integer :: k
 
  if (use_gauss_legendre) then
+    !print*,' lsum analytically = ',l_sum(:)
     do k=1,n
        l_sum(k) = integrate_gauss_legendre(size(x),weights,integrand(x,lamb,k-1,logx=logx)) - mu(k)
     enddo
+    !print*,' lsum numerically = ',l_sum(:)
  else
     if (use_log_grid) then
        if (log_grid_is_uniform) then
@@ -165,7 +167,18 @@ function integrand(x,lamb,k,logx) result(y)
  integer, intent(in) :: k
  real( kind = rk ) :: xi(size(lamb)), term
  integer :: i,j
+ real, parameter :: e100 = exp(-100.)
+ real :: prefac,theta,d_on_p,p,beta,d
 
+ beta = lamb(1)   ! overall normalisation factor
+ theta = lamb(2)  ! scale parameter theta on wikipedia
+ d_on_p = lamb(3)      ! shape parameter k on wikipedia
+ p = lamb(4)      ! shape parameter
+ prefac = beta * p / (theta * Gamma(d_on_p))
+ d = d_on_p * p
+ !prefac = beta / (theta * Gamma(d/p) * (1./(p*theta))**(1./p))
+ !print*, 1./Gamma(d/p) !prefac,beta,a,d,p
+ !read*
  do i=1,size(x)
     if (use_log_grid) then
        if (present(logx)) then
@@ -185,11 +198,21 @@ function integrand(x,lamb,k,logx) result(y)
     ! function is exp( lam_0 + lam_1 x + lam_2 x^2 + ...)
     ! or if log_grid is set exp( lam_0 + lam_1 log(x) + lam_2 log(x)^2 + ...)
     term = dot_product(lamb,xi)
-    if (term < -50.) then ! avoid wasted effort on exp(-100) etc
-       y(i) = 0.
+    if (term < -100.) then ! avoid wasted effort on exp(-100) etc
+       y(i) = x(i)**(k/3.)*e100
     else
        y(i) = x(i)**(k/3.)*exp(term)
     endif
+    !if (.not.present(logx)) stop 'logx is not present'
+
+    ! GENERALIZED GAMMA DISTRIBUTION (standard Gamma distribution if p=1)
+    term = -(x(i)/theta)**p
+    if (term < -100.) then
+       y(i) = x(i)**(k/3.)*e100*prefac !prefac*(x(i)/theta)**(d-1)*e100
+    else
+       y(i) = x(i)**(k/3.)*prefac*(x(i)/theta)**(d-1)*exp(term)
+    endif
+    !x(i)**(lamb(2)-1)*exp(-lamb(3)*x(i))*prefac
     !y(i) = x(i)**(k/3.) * exp(max(dot_product(lamb,xi),-100.))
  enddo
 
@@ -218,5 +241,67 @@ function fsolve_error(ierr) result(string)
  end select
 
 end function fsolve_error
+
+!
+! maximum entropy reconstruction of function given moments
+!
+subroutine reconstruct_gamma_dist(mu,lambsol,lsum,ierr,lambguess)
+ real, intent(in) :: mu(:)
+ real, intent(out) :: lambsol(size(mu))
+ real, intent(out) :: lsum(size(mu))
+ integer, intent(out) :: ierr
+ real, intent(in), optional :: lambguess(size(mu))
+ integer :: n_moments,k
+ real, parameter :: tol = 1.e-6
+ real :: d_on_p,theta,p
+
+ lambsol = 0.
+ ierr = 0
+ n_moments = 2 !size(mu)
+
+ ! initial guesses for Lagrange multipliers
+ if (present(lambguess)) then
+    lambsol = lambguess
+ else
+    lambsol(1) = 1.7
+ endif
+
+ call fsolve(residual_fit_gamma,n_moments,lambsol,lsum,tol,ierr)
+
+ d_on_p = lambsol(1)
+ p = lambsol(2)
+ theta = (mu(2)/mu(1) * Gamma(d_on_p) / Gamma(d_on_p + 1./(3.*p)))**3
+ !print*,'INPUT  moments = ',mu
+ !print*,'SOLVED moments = ',(mu(1)*theta**(k/3.)*Gamma(d_on_p + k/(3.*p))/Gamma(d_on_p),k=0,3)
+
+! call residual_fit_gamma(n_moments,lambsol,lsum)
+
+contains
+
+subroutine residual_fit_gamma(n,lamb,l_sum)
+ integer, intent(in) :: n
+ real ( kind = rk ), intent(in)  :: lamb(n) ! guess for  solution
+ real ( kind = rk ), intent(out) :: l_sum(n) ! function evaluated for given lamb
+ real ( kind = rk ) :: scalefac, p, theta, d_on_p
+ integer :: k
+
+ d_on_p = lamb(1)
+ p = lamb(2)
+ theta = (mu(2)/mu(1) * Gamma(d_on_p) / Gamma(d_on_p + 1./(3.*p)))**3
+
+ ! solve for moments 3 and 4 (k=2,3)
+ l_sum(1) = theta**(2./3.) * Gamma(d_on_p + 2./(3.*p)) / Gamma(d_on_p) * mu(1)/mu(3) - 1.! /mu(1)
+ l_sum(2) = theta**(3./3.) * Gamma(d_on_p + 3./(3.*p)) / Gamma(d_on_p) * mu(1)/mu(4) - 1.! /mu(1)
+
+ !print*,'d/p,p = ',d_on_p,p,' want moments = ',mu(3:4), ' got ',&
+ !       (theta**(k/3.) * Gamma(d_on_p + k/(3.*p)) / Gamma(d_on_p) * mu(1),k=2,3),' err = ',l_sum(1:2)
+ !do k=1,4
+ !   l_sum(k) = scalefac * theta ** (1.+(k-1)/3.) / lamb(2) * Gamma(d_on_p + (k-1)/3.) - mu(k)
+! enddo
+
+end subroutine residual_fit_gamma
+
+end subroutine reconstruct_gamma_dist
+
 
 end module reconstruct_from_moments
